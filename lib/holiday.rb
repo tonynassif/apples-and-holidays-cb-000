c08de780805d09ae10d59d76d2b2ae2fc0645244
Tony Nassif <tonynassif@gmail.com>
2017-05-31 16:57:41 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,17 +29,26 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |season, holiday|
+    holiday.each do |attribute, value|
+      if attribute == :christmas || attribute == :new_years
+       holiday[attribute].push(supply)
+      end
+    end
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash.each do |season, holiday|
+    holiday.each do |attribute, value|
+      if attribute == :memorial_day
+       holiday[attribute].push(supply)
+      end
+    end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
